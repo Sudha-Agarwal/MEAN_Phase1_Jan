cb633f3c07cc0c912773a1bcd6278d4bf3ef097f
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-01-29 09:56:10 +0530

    commit done on 29/1/23

--- js/constructor_function.js
+++ js/constructor_function.js
@@ -10,7 +10,51 @@ function f2(name,age){
 }
 
 f2("Sudha",30);//function calling with 2 arguments
-function Person(name, age){
+
+function Person(name1, age1){
+    this.name = name1;
+    this.age = age1;
+    this.speak = function(){
+        console.log(this.name + " is speaking");
+    }
+}
+var obj1 = new Person("Sudha",40);
+
+console.log(obj1.name);
+obj1.speak();
+
+
+var obj2 = new Person("Sudha1",30);
+console.log(obj2.name);
+
+class Account {
+    constructor(balance, customerId, type) {
+        this.balance = balance;
+        this.customerId = customerId;
+        this.type = type;      
+
+        this.deposit = function (amount) {
+            this.bal = this.bal + amount;
+        };
+        this.withdraw = function (amount) {
+            this.bal = this.bal - amount;
+        };
+        this.getBal = function () {
+        };
+    }
 }
 
-var obj1 = new Person();
+class SavingsAccount extends Account{   
+    constructor(roi){
+        //this.bal = bal;//no need for this
+        this.roi = roi;
+        this.calculateInterest = function(){
+            //calculate interest
+        }
+    } 
+}
+class DematAccount extends Account{    
+
+}
+
+new SavingsAccount();
