c2a997f97d6662ec5e7dd487e531239a9cc945d6
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-02-04 10:05:00 +0530

    first commit on 04-02-23

--- js/callback.js
+++ js/callback.js
@@ -1,3 +1,4 @@
+/*
 function one(var1, callback){
     //console.log(callback);
     callback(var1);
@@ -12,3 +13,37 @@ one("parameter1", two);
 //read operation - it will take some time
 //print data
 //some js statements
+
+
+
+
+*/
+function one(callback){
+    console.log("One");
+    callback("param2");//function calling
+}
+function two(param2){
+    console.log(param2);
+    console.log("Two");
+}
+function three(){
+    console.log("Three");
+}
+//First way
+//one(two);
+//second way
+one(function(param2){
+    console.log(param2);
+    console.log("Two");
+});
+
+//two();
+
+//function definition
+var x1 = function(z){
+    console.log("function with value: " + z);
+    return 1;
+}
+
+var y = x1("z");//function calling
+
