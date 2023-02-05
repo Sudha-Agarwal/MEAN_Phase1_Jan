325f1e6a657540d16b762ea4c38256bc652784e8
Sudha Agarwal <sudha.agarwal84@gmail.com>
2023-02-05 12:58:06 +0530

    commit done on 05-02-23

---
+++ js/promise.js
@@ -0,0 +1,42 @@
+function myAsyncFunction(name){    
+    var promise = new Promise(function(resolve1,reject1){
+        if(name == "Sudha"){
+            resolve1("Promise resolved");
+        }
+        else {
+            reject1("Promise rejected");
+        }
+    })
+    return promise;
+}
+function resolvepromise(data){
+    console.log(data);
+}
+function rejectpromise(data){
+    console.log(data);
+}
+//first way
+//myAsyncFunction("Sudha1").then(resolvepromise).catch(rejectpromise);
+
+//second way
+/*myAsyncFunction("Sudha").then(function(data){
+    console.log(data);
+}).catch(function(data){
+    console.log(data);
+});*/
+
+//myAsyncFunction("Sudha").then(resolvepromise, rejectpromise);
+
+/*myAsyncFunction("Sudha1").then(function(data){
+    console.log(data);
+},
+function(data){
+    console.log(data);
+}
+)*/
+
+//with arrow function
+myAsyncFunction("Sudha").then(
+    data => console.log(data),
+    error=>console.log(error)
+);
